886dd9b755dc15d6b23c7c435a9258f33233b2b8
Andre Madarang <andre_madarang@hotmail.com>
2019-05-20 01:09:07 -0400

    Add link to Tailwind v1.0 examples

--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -12,7 +12,9 @@
     <div class="mt-24">
       <svg class="h-48" viewBox="0 0 90 90" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><defs><circle id="b" cx="40" cy="40" r="40"></circle><filter x="-8.8%" y="-6.2%" width="117.5%" height="117.5%" filterUnits="objectBoundingBox" id="a"><feOffset dy="2" in="SourceAlpha" result="shadowOffsetOuter1"></feOffset><feGaussianBlur stdDeviation="2" in="shadowOffsetOuter1" result="shadowBlurOuter1"></feGaussianBlur><feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.1 0" in="shadowBlurOuter1"></feColorMatrix></filter><linearGradient x1="0%" y1="0%" y2="100%" id="c"><stop stop-color="#2383AE" offset="0%"></stop><stop stop-color="#6DD7B9" offset="100%"></stop></linearGradient></defs><g fill="none" fill-rule="evenodd"><g transform="translate(5 5)"><use fill="#000" filter="url(#a)" xlink:href="#b"></use><use fill="#FFF" xlink:href="#b"></use></g><path d="M25.6 33.92C27.52 26.24 32.32 22.4 40 22.4c11.52 0 12.96 8.64 18.72 10.08 3.84.96 7.2-.48 10.08-4.32-1.92 7.68-6.72 11.52-14.4 11.52-11.52 0-12.96-8.64-18.72-10.08-3.84-.96-7.2.48-10.08 4.32zM11.2 51.2c1.92-7.68 6.72-11.52 14.4-11.52 11.52 0 12.96 8.64 18.72 10.08 3.84.96 7.2-.48 10.08-4.32-1.92 7.68-6.72 11.52-14.4 11.52-11.52 0-12.96-8.64-18.72-10.08-3.84-.96-7.2.48-10.08 4.32z" fill="url(#c)" transform="translate(5 5)"></path></g></svg>
     </div>
-    <h1 class="mb-10">Tailwind CSS Examples</h1>
+    <h1>Tailwind CSS v0.74 Examples</h1>
+    <p class="mb-10">For Tailwind v1.0 examples, <a href="https://tailwind-v1-examples.netlify.com" class="text-blue hover:text-blue-dark">go here</a>.</p>
+
     <div class="flex flex-wrap items-center justify-between w-3/5 mx-auto mb-10">
       <div class="item-twitter w-64 flex items-center mx-auto mb-8 pr-8">
         <div class="mr-4">
